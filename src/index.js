1f2bf7e8c76612b88409399976fcb6671bcef881
Alexandr Shushunov <ashushunov@mail.ru>
2018-05-02 19:34:38 +0300

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {combineReducers, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {devToolsEnhancer} from 'redux-devtools-extension';
 import {App} from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(
+    combineReducers({
+        foo: state => state || {}
+    }),
+    devToolsEnhancer()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
