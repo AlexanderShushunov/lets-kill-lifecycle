10a3a1c8e3d58823aa8aa54091f7033662b2b4ba
Alexandr Shushunov <ashushunov@mail.ru>
2018-05-02 19:27:59 +0300

    Add prettier

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import * as React from 'react';
 import './App.css';
 
 export class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        Hi!
-      </div>
-    );
-  }
+    render() {
+        return <div className="App">Hi!</div>;
+    }
 }
