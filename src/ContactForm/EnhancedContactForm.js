714c19c5bd813ab06da7a1b13b7a41d3438470ec
Alexandr Shushunov <ashushunov@mail.ru>
2018-11-05 18:54:12 +0300

    Add temp value for phone (hook)

--- src/ContactForm/EnhancedContactForm.js
+++ src/ContactForm/EnhancedContactForm.js
@@ -1,28 +1,54 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {connect} from 'react-redux';
 import {ContactForm} from './ContactForm';
-import {getFirstName, isNotEmailTouched} from './selectors';
-import {changeEmail} from './actions';
+import {
+    getEmployed,
+    getFirstName,
+    getPhone,
+    isNotEmailTouched
+} from './selectors';
+import {changeEmail, changePhone} from './actions';
 
 export const EnhancedContactForm = connect(
     state => ({
         firstName: getFirstName(state),
-        isNotEmailTouched: isNotEmailTouched(state)
+        isNotEmailTouched: isNotEmailTouched(state),
+        employed: getEmployed(state),
+        phone: getPhone(state)
     }),
     {
-        changeEmail
+        changeEmail,
+        changePhone
     }
-)(({changeEmail, firstName, isNotEmailTouched}) => {
-    useEffect(
-        () => {
-            if (isNotEmailTouched) {
-                changeEmail(calcEmail(firstName));
-            }
-        },
-        [firstName]
-    );
-    return <ContactForm />;
-});
+)(
+    ({
+        changeEmail,
+        firstName,
+        isNotEmailTouched,
+        employed,
+        phone,
+        changePhone
+    }) => {
+        useEffect(
+            () => {
+                if (isNotEmailTouched) {
+                    changeEmail(calcEmail(firstName));
+                }
+            },
+            [firstName]
+        );
+
+        const tempPhone = useRef();
+        useEffect(
+            () => {
+                changePhone(tempPhone.current);
+                tempPhone.current = phone || '';
+            },
+            [employed]
+        );
+        return <ContactForm />;
+    }
+);
 
 const calcEmail = firstName => {
     if (!firstName) {
